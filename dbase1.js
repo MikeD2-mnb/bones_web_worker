7cbd09a758e4dc8be0dfcca56d28cb1953ed83b6
MikeD2-mnb <62773667+MikeD2-mnb@users.noreply.github.com>
2024-10-14 08:44:31 +1200

    Add files via upload
    initial, mostly working

---
+++ dbase1.js
@@ -0,0 +1,19 @@
+function timedCount() {
+    console.log("making fetch");
+    fetch('//192.168.0.52:3050/data',{mode:'no-cors'})
+        .then(response => response.json())
+        .then(data => {
+            console.log(response);
+            postMessage(data);  //send data back to the main thread
+        })
+        .catch(error => {
+            console.log("error",error);
+            console.error(error);
+            postMessage("[  {  Dev: 0, Meas: 'Volt'}]"); //'Error fetching data');
+        });
+        //setTimeout(timedCount, 200);
+}
+//while (1){
+timedCount();
+
+//}
---
+++ dbase_server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const port = 3050; //you can change this port to whatever port you want
+
+const db = new sqlite3.Database('/home/debian/examples/C_BMS/test2.db', sqlite3.OPEN_READONLY);
+//app.use(express.static("/var/www/html/templates"));
+//make an endpoint to fetch data
+app.get('/data', (req, res) => {
+    console.log(".get received");
+    db.all("SELECT * FROM EpiData", [], (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        
+        res.json(rows);
+        console.log(rows);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
