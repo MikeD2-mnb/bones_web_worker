675ce63e01cdf5133804f15497206b058947fd41
MikeD2-mnb <62773667+MikeD2-mnb@users.noreply.github.com>
2024-10-14 15:10:49 +1200

    Update dbase_server.js

--- dbase_server.js
+++ dbase_server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-
+const cors = require('cors');
 const app = express();
 const port = 3050; //you can change this port to whatever port you want
 
 const db = new sqlite3.Database('/home/debian/examples/C_BMS/test2.db', sqlite3.OPEN_READONLY);
-//app.use(express.static("/var/www/html/templates"));
+app.use(express.static("/var/www/html/templates"));
 //make an endpoint to fetch data
 app.get('/data', (req, res) => {
     console.log(".get received");
